02b657db3a8fc85e86723d99c0493561b8e7d874
applegit11 <13774437301@163.com>
2018-07-26 10:49:38 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+access_key = 'xxx'
+access_secret = 'xxx'
+
+
+backup_dir = '/tmp'
+
+clear = 30
+backup_instances = []
+
+a = {
+        'DBInstanceId': 'xxx',
+        'Engine': 'MySQL',
+        'BackupMethod': 'Logical',
+        'BackupDatabases': 'db'
+    }
